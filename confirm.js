9e4e3bf71e0c4d4188b1c43818db9183e4162a55
Calvin Heend <calvinheend@gmail.com>
2019-03-02 02:53:32 -0600

    add files

---
+++ confirm.js
@@ -0,0 +1,42 @@
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+    
+    var button = document.getElementById("submitPassword");
+if(button){
+    console.log('On Loaded')
+    button.addEventListener('click',function(){
+    var val = document.getElementById("password").value;    
+    var val2 = document.getElementById("confirmPassword").value;    
+    console.log('ON CLICK',val,val2);
+        
+        if (val === val2){
+            
+            console.log('Password Confirm')
+        }
+        else 
+            {
+                console.log('Passwords do not match')
+            }
+        
+});
+} else {
+    console.log('Not Working')
+}
+})
+//console.log(password)
+            
+  //          function validatePassword()
+
+    //        {
+      //      if(password.value != confirm_password.value) 
+            
+        //    {
+            
+          //  confirm_password.setCustomValidity("Passwords Don't Match.");
+            
+            //}
+            //else {
+            //confirm_password.setCustomValidity('');
+            //}
+        //}
