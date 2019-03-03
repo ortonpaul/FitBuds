ddc41f5f93ab45fc57f75878a1ee6237a1567719
Paul Orton <ortonpaul18@gmail.com>
2019-03-02 22:18:12 -0600

    Added name stuff

--- member.php
+++ member.php
@@ -13,8 +13,16 @@
 		exit("You are not logged in. Redirecting..."); //Kicks off and automatically closes MySQL connection
 	}
 	$username = htmlentities($_SESSION['usernamev3']);
-	// echo "<p>Welcome to the logged in area, {$username}!</p>";
+  $name = " ";
+  $checkUser =  mysqli_prepare($databaseSQL, "SELECT Name FROM users WHERE Email=?;");
+  mysqli_stmt_bind_param($checkUser, 's', $username);
 
+  mysqli_stmt_execute($checkUser); //System of prepared execution prevents SQL Injection
+  if ($result = mysqli_stmt_get_result($checkUser)) {	// echo "<p>Welcome to the logged in area, {$username}!</p>";
+    if ($row = mysqli_fetch_row($result)) {
+      $name = $row[0];
+    }
+}
 	echo '	<body>
 			<div class="homeIcon" id="selected">
 					<a href="member.php"><img src="pages/assets/home.png" alt="home" ></a>
@@ -26,7 +34,7 @@
 					<li><a href="resources.php">Resources</a></li>
 			</ul>
 			<div>
-					<h1 style="padding-top: 100px">Welcome back, {$username}!</h1>
+					<h1 style="padding-top: 100px">Welcome back, ' . $name . '!</h1>
 					<h3>Click a tab to get started!</h3>
 			</div>';
 	echo '<button onClick="javascript:window.location.href=\'logout.php\'">Logout</button>';
